4426df877ecb98bfd039ac091e442fd5f478e773
printf100 <mickey0502@naver.com>
2020-04-09 21:39:24 +0900

    20200409 ajax crawling (CORS)

--- Python05/naver/app.py
+++ Python05/naver/app.py
@@ -4,18 +4,15 @@ from flask import Flask, render_template
 import requests
 from bs4 import BeautifulSoup
 import json
-from flask_cors import CORS, cross_origin
+import flask_cors
 
 app = Flask(__name__)
-CORS(app, resources={r'*':{'origins':'*'}})
+flask_cors.CORS(app)
 
 
 @app.route('/')
 def root():
     return render_template('index.html')
-
-
-lst = list()
 
 
 @app.route('/crawling')
@@ -26,23 +23,20 @@ def crawling():
     
     movies = soup.find_all('dl', class_='lst_dsc')
     
+    lst = list()
     for movie in movies:
-        title = movie.find('a').text
-        star = movie.find('span', class_='num').text
-        
         tmp = {}
-        tmp['title'] = title
-        tmp['star'] = star
+        tmp['title'] = movie.find('a').text
+        tmp['star'] = movie.select('.num')[0].text
         
         lst.append(tmp)
-    
+        
     # crawling된 데이터를 dictionary에 {movies:[{title: 제목, star: 별점}, ...]} 로 저장하자.
-    res = {}
-    res['movies'] = lst
+    res_dict = {'movies':lst}
     
-    res_json = json.dumps(res, ensure_ascii=False)
+    res_json = json.dumps(res_dict, ensure_ascii=False)
     
-    # json으로 변환하여 리턴하자        
+    # json으로 변환하여 리턴하자
     return res_json
 
 
